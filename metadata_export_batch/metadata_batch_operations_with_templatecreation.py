ba9f428ba7699981fcf4809be26009fa82eea60b
cmartin616 <cmartin616@gmail.com>
2014-09-15 15:46:46 -0500

    Tweaked formatting, added ability to run on set of records.

--- metadata_export_batch/metadata_batch_operations_with_templatecreation.py
+++ metadata_export_batch/metadata_batch_operations_with_templatecreation.py
@@ -13,88 +13,64 @@ def getDrivePath():
             break
     return drivePath
 
-userName = os.environ.get('USERNAME')
-
-if os.path.exists(r'D:\drive\\'):
-    drivePath = r'D:\drive\Map Library Projects\MGS'
-elif os.path.exists(os.path.join(r'C:\Users\\',userName,'Google Drive')):
-    drivePath = os.path.join(r'C:\Users\\',userName,'Google Drive\Map Library Projects\MGS')
-else:
-    drivePath = getDrivePath()
-
-#Get the record and path to XMLs
-record = raw_input("Enter record number: ")
-record_path = raw_input("Enter path to shapefiles: ")
-blankMXD = r'D:\drive\Map Library Projects\MGS\Metadata Templates\blank.mxd'
-
-#Static vars
-TRANSLATOR = "C:\\Program Files\\ArcGIS\\Desktop10.2\\Metadata\\Translator\\ARCGIS2FGDC.xml"
-ISOTRANSLATOR = "C:\\Program Files\\ArcGIS\\Desktop10.2\\Metadata\\Translator\\ARCGIS2ISO19139.xml"
-base_path = os.path.join(drivePath,"Records")
-
-#set workspace
-arcpy.env.workspace = os.path.join(base_path, record, record_path)
-ws = arcpy.env.workspace
-
-#create final_XMLs dir if it doesn't already exist
-if os.path.exists(os.path.join(base_path,record,"final_XMLs")) == False:
-    arcpy.CreateFolder_management(os.path.join(base_path,record),"final_XMLs")
-
-#set the output path for export metadata function
-OUTPUT = os.path.join(base_path,record,"final_XMLs")
-
 def import_XML():
 
     #baseSHP = raw_input('Enter shapefile to use as template: ')
     #shpPath = os.path.join(base_path, record,record_path,baseSHP)
-    templatePath = os.path.join(base_path,record,'final_XMLs\template\template.xml')
+    templatePath = os.path.join(base_path,record,'final_XMLs\\template.xml')
     #print shpPath
 
-    arcpy.ExportMetadata_conversion (shpPath, ISOTRANSLATOR,
-    templatePath)
+    # Used to explort a shapefile as a default template
+    '''arcpy.ExportMetadata_conversion (shpPath, ISOTRANSLATOR,
+    templatePath)'''
 
-    #get a list of all the SHPs
-    files = arcpy.ListFiles("*.shp")
 
-    totalTimeTic = time.time()
+    for root, dir, files in os.walk(os.path.join(base_path, record)):
+        #set workspace
+        arcpy.env.workspace = root
 
-    #loop through SHPs and import the metadata for each
-    for f in files:
+        #get a list of all the SHPs
+        files = arcpy.ListFiles("*.shp")
 
-        shapefilePath = os.path.join(base_path,record,record_path,f)
+        totalTimeTic = time.time()
 
-        tic = time.time()
-        print 'Trying to import XML to: ', f
-        arcpy.ImportMetadata_conversion (templatePath,"FROM_ESRIISO",f, "DISABLED")
+        #loop through SHPs and import the metadata for each
+        for f in files:
 
-        # Trying to get thumbnail, postponing for now.
-        """# get the map document
-        mxd = arcpy.mapping.MapDocument(blankMXD)
-        # get the data frame
-        df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
-        # create a new layer
-        newlayer = arcpy.mapping.Layer(shapefilePath)
-        # add the layer to the map at the bottom of the TOC in data frame 0
-        arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
-        print "creating thumbnail for " + f
-        mxd.makeThumbnail()
-        mxd.save()
-        arcpy.mapping.RemoveLayer(df, newlayer)
-        mxd.save()"""
+            #shapefilePath = os.path.join(base_path,record,record_path,f)
 
-        toc = time.time()
-        s = toc-tic
+            tic = time.time()
+            print 'Trying to import XML to: ', f
+            arcpy.ImportMetadata_conversion (templatePath,"FROM_ESRIISO",f, "DISABLED")
+
+            # Trying to get thumbnail, postponing for now.
+            """# get the map document
+            mxd = arcpy.mapping.MapDocument(blankMXD)
+            # get the data frame
+            df = arcpy.mapping.ListDataFrames(mxd,"*")[0]
+            # create a new layer
+            newlayer = arcpy.mapping.Layer(shapefilePath)
+            # add the layer to the map at the bottom of the TOC in data frame 0
+            arcpy.mapping.AddLayer(df, newlayer,"BOTTOM")
+            print "creating thumbnail for " + f
+            mxd.makeThumbnail()
+            mxd.save()
+            arcpy.mapping.RemoveLayer(df, newlayer)
+            mxd.save()"""
+
+            toc = time.time()
+            s = toc-tic
+            m, s = divmod(s, 60)
+            h, m = divmod(m, 60)
+            timeFormat = "%d:%02d:%02d" % (h, m, s)
+            print 'Time elapsed: ',timeFormat
+
+        totalTimeToc = time.time()
+        s = totalTimeToc-totalTimeTic
         m, s = divmod(s, 60)
         h, m = divmod(m, 60)
         timeFormat = "%d:%02d:%02d" % (h, m, s)
-        print 'Time elapsed: ',timeFormat
-
-    totalTimeToc = time.time()
-    s = totalTimeToc-totalTimeTic
-    m, s = divmod(s, 60)
-    h, m = divmod(m, 60)
-    timeFormat = "%d:%02d:%02d" % (h, m, s)
-    print 'Total time elapsed: ',timeFormat
+        print 'Total time elapsed: ',timeFormat
 
 def export_xml():
     #set the output path for export metadata function
@@ -103,6 +79,7 @@ def export_xml():
     #get a list of all the XMLs
     files = arcpy.ListFiles("*.xml")
     print files
+    raw_input()
 
     #loop through XMLs and export the metadata for each to the final_XMLs directory
     for f in files:
@@ -133,10 +110,43 @@ def export_xml():
             os.path.join(OUTPUT, f[:f.find(".")]+ ".xml"))
 
 
-imp_xml = raw_input('Do you want to batch import XML? ')
+userName = os.environ.get('USERNAME')
+
+if os.path.exists(r'D:\drive\\'):
+    drivePath = r'D:\drive\Map Library Projects\MGS'
+elif os.path.exists(os.path.join(r'C:\Users\\',userName,'Google Drive')):
+    drivePath = os.path.join(r'C:\Users\\',userName,'Google Drive\Map Library Projects\MGS')
+else:
+    drivePath = getDrivePath()
+
+#Static vars
+TRANSLATOR = "C:\\Program Files\\ArcGIS\\Desktop10.2\\Metadata\\Translator\\ARCGIS2FGDC.xml"
+ISOTRANSLATOR = "C:\\Program Files\\ArcGIS\\Desktop10.2\\Metadata\\Translator\\ARCGIS2ISO19139.xml"
+base_path = os.path.join(drivePath,"Records")
+
+""" USE THIS TO MANUALLY INPUT SHAPEFILES/RECORDS """
+
+'''#Get the record and path to XMLs
+record = raw_input("Enter record number: ")
+record_path = raw_input("Enter path to shapefiles: ")
+blankMXD = r'D:\drive\Map Library Projects\MGS\Metadata Templates\blank.mxd'
+
+#create final_XMLs dir if it doesn't already exist
+if os.path.exists(os.path.join(base_path,record,"final_XMLs")) == False:
+    arcpy.CreateFolder_management(os.path.join(base_path,record),"final_XMLs")
+
+#set the output path for export metadata function
+OUTPUT = os.path.join(base_path,record,"final_XMLs")'''
+
+""" USE THIS FOR A SET OF RECORDS TO RUN """
+for x in [167,191,57175,57181,57188,57241,57702,58175]:
+    record = str(x)
+    import_XML()
+
+'''imp_xml = raw_input('Do you want to batch import XML? ')
 if imp_xml.lower() in ('yes', 'y'):
     import_XML()
 
 exp_xml = raw_input('Do you want to batch export XML? ')
 if exp_xml.lower() in ('yes','y'):
-    export_xml()
+    export_xml()'''
