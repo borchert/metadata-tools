3f3ac9e5bead15a913359977b022fc8286527f02
Kevin Dyke <krdyke@gmail.com>
2014-09-18 13:33:51 -0500

    Update defineProjections.py

--- defineProjections.py
+++ defineProjections.py
@@ -7,47 +7,49 @@ import sys
 from pprint import pprint
 import pdb
 
-e00s = [317,321,406,414,422,
-           432,440,514,518,600,
-           604,928,936,56995,56996,
-           56997,57000,57002,57004,
-           57172,57186]
-
-
-ROOT = ''
+records = [108814,109019,116056,116097,123383,167,191,203,210,290,306,310,321,406,414,422,432,440,514,518,56995,56997,57000,57002,57004,57172,57174,57177,57178,57181,57182,57186,57241,57242,57440,57694,57695,57699,57702,58041,58042,58162,58164,58165,58175,58233,58513,58549,58551,58554,58560,58716,58719,58781,58804,59648,59765,59783,59784,604,928,92939,92941,936,973,98055]
+ROOT = ""
 
 install_dir = arcpy.GetInstallInfo("desktop")["InstallDir"]
 translator = os.path.join(install_dir,  "Metadata\Translator\ARCGIS2FGDC.xml")
 
-for i in e00s[0:1]:
+for i in records:
     unknowns = []
     srs = {}
     
-    ws = os.path.join(ROOT,str(i),"converted","GISfiles")
+    #ws = os.path.join(ROOT,str(i),"converted","GISfiles")
+    ws = os.path.join(ROOT,str(i))
     xml_path = os.path.join(ROOT,str(i),"final_XMLs")
     walk = arcpy.da.Walk(ws,datatype=["FeatureClass","RasterDataset"])
 
-    for dirpath,dirname,files in walk:
-        for f in files:
-            d = os.path.join(dirpath,f)
-            desc = arcpy.Describe(d)
-            if desc.datatype.startswith('ShapeFile'):
-                if desc.spatialreference:
-                    sr = desc.spatialreference.PCSCode
-                    if sr == 0:
-                        unknowns.append(d)
-                else:
-                    print f,'has no spatial ref'
-                if srs.has_key(sr):
-                    srs[sr] = srs[sr] + 1
-                else:
-                    srs[sr] = 1
+    if not os.path.exists(os.path.join(ws,"converted","GISfiles")):   
+        for dirpath,dirname,files in walk:
+            for f in files:
+                d = os.path.join(dirpath,f)
+                desc = arcpy.Describe(d)
+                if desc.datatype.startswith('ShapeFile'):
+                    if desc.spatialreference:
+                        sr = desc.spatialreference.PCSCode
+                        if sr == 0:
+                            unknowns.append(d)
+                        if srs.has_key(sr):
+                            srs[sr] = srs[sr] + 1
+                        else:
+                            srs[sr] = 1
+                    else:
+                        print f,'has no spatial ref'
                     
-    #see https://wiki.python.org/moin/HowTo/Sorting for more on sorting iterables
-    most_popular_srs = sorted(srs.iteritems(), reverse=True, key=lambda item: item[1])[0][0]
-    new_srs = arcpy.SpatialReference(most_popular_srs)
-    print new_srs.name,'is the most popular SRS for', i
-    
+                    
+        #see https://wiki.python.org/moin/HowTo/Sorting for more on sorting iterables
+        most_popular_srs = sorted(srs.iteritems(), reverse=True, key=lambda item: item[1])[0][0]
+
+        #create spatial reference object using the code
+        new_srs = arcpy.SpatialReference(most_popular_srs)
+        print new_srs.name,'is the most popular SRS for', i
+
+    else:
+        print 'skipped %s because it has e00s' % (str(i))
+        
     for d in unknowns:
         arcpy.DefineProjection_management(d, new_srs)
         print 'defined projection for' ,d
@@ -58,7 +60,10 @@ for i in e00s[0:1]:
             print 'removed existing XML to make room for a new one'
         arcpy.ExportMetadata_conversion(d, translator,
                                         path_to_xml)
-        print 'exported new XML for', d,'\n'
+        print 'exported new XML for', d
+
+    print '\n'
 
 
                                         
+
