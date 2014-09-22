5c420b1b80bdc3b2e57b6630d2520e65b2ff5a55
cmartin616 <cmartin616@gmail.com>
2014-09-22 15:38:43 -0500

    Minor tweaks.

--- move_rename_meta/move_rename_meta.py
+++ move_rename_meta/move_rename_meta.py
@@ -1,22 +1,8 @@
 import os
 import shutil
+import find_Drive
 
-def getDrivePath():
-    while True:
-        drivePath = raw_input("Please enter the path to your Drive folder (i.e. D:\drive or C:\Users\username\Google "
-                              "Drive):  ")
-        if not os.path.exists(drivePath):
-            print 'That path does not work.  Please try again.'
-        else:
-            break
-    return drivePath
-
-if os.path.exists(r'D:\drive\\'):
-    drivePath = r'D:\drive\Map Library Projects'
-elif os.path.exists(os.path.join(r'C:\Users\\',userName,'Google Drive')):
-    drivePath = os.path.join(r'C:\Users\\',userName,'Google Drive\Map Library Projects')
-else:
-    drivePath = getDrivePath()
+drivePath = find_Drive.main()
 
 startDir = r'C:\Users\mart3565\Downloads\pub'
 outPathDir = os.path.join(drivePath, 'MN Geospatial Commons\\data\\renamed_metadata_xmls')
@@ -39,4 +25,3 @@ for root, dirs, files in os.walk(startDir):
             print 'WRITING -', outFile
 
             shutil.copy(os.path.join(root,f), outFile)
-
