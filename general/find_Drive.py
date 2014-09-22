764c3f6f97faf1b2f0dc019672c770a0f578da9d
cmartin616 <cmartin616@gmail.com>
2014-09-22 15:37:30 -0500

    Changed drive name (replace space with _ )

--- general/find_Drive.py
+++ general/find_Drive.py
@@ -2,9 +2,9 @@ import os
 
 def main():
     if os.path.exists(r'D:\drive\\'):
-        drivePath = r'D:\drive\Map Library Projects'
+        drivePath = r'D:\drive\Map_Library_Projects'
     elif os.path.exists(os.path.join(r'C:\Users\\',userName,'Google Drive')):
-        drivePath = os.path.join(r'C:\Users\\',userName,'Google Drive\Map Library Projects')
+        drivePath = os.path.join(r'C:\Users\\',userName,'Google Drive\Map_Library_Projects')
     else:
         while True:
             drivePath = raw_input("Please enter the path to your Drive folder (i.e. D:\drive or C:\Users\username\Google "
