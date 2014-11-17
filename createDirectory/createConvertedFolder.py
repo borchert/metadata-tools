6e6dc1ae097b175eee97266468568696235fb708
cmartin616 <cmartin616@gmail.com>
2014-11-17 10:24:53 -0600

    Added comments.

--- createDirectory/createConvertedFolder.py
+++ createDirectory/createConvertedFolder.py
@@ -1,8 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ---------------------------------------------------------------------------
+# createConvertedFolder.py
+# Created on: 2014-07-07 
+# Description: Creates a folder in every directory 1 level deep
+# ---------------------------------------------------------------------------
+
+# This script will create a directory (set up for /converted) in every
+# directory found, limited to one level deep.
+
 import os
 
 startDir = os.getcwd()
 startDepth = startDir.count(os.sep)
 newDir = r'/converted'
+
+print 'Beginning search.'
 
 for root, dirs ,files in os.walk(startDir):
  
@@ -18,6 +32,6 @@ for root, dirs ,files in os.walk(startDir):
         if not os.path.exists(makeDir):
             os.makedirs(makeDir)
             print 'Created: ' + str(makeDir)
-            #raw_input()
+
         else:
             print root + '   --  Exists and skipped'
